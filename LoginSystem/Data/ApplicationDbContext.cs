447724ed387a16e31ffee8c29a945aaccc91e0ae
Amir Zarei <amir.zarei9288@gmail.com>
2025-08-02 11:06:19 +0200

    Step 1: Complete backend foundation setup
       - Add Entity Framework packages
       - Create User model with all properties and data annotations
       - Create ApplicationDbContext
       - Configure database connection with LocalDB
       - Create and apply initial migration
       - Database and Users table created successfully

---
+++ LoginSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using LoginSystem.Models;
+
+namespace LoginSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+} 
--- LoginSystem/Program.cs
+++ LoginSystem/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using LoginSystem.Data;
+
 namespace LoginSystem
 {
     public class Program
@@ -12,6 +15,10 @@ namespace LoginSystem
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // Add DbContext to the container
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
